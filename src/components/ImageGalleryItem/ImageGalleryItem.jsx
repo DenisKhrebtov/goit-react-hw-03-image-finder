4678fd8ff3efeb3622055e1777e149e76fec7cde
DenisKhrebtov <khrebtovdenis@gmail.com>
2022-10-31 23:41:59 +0200

    *

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,5 +1,5 @@
 export const ImageGalleryItem = ({ image }) => {
-  const { webformatURL, largeImageURL, tags } = image;
+  const { webformatURL, tags } = image;
   return (
     <li>
       <img src={webformatURL} alt={tags} />
